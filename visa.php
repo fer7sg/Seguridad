465eb31edcf74a121a5e1c24983e00ac8df5edb5
fer7sg <fer_sanchez_25_07@hotmail.com>
2017-05-17 10:46:29 -0500

    Update visa.php

--- visa.php
+++ visa.php
@@ -22,36 +22,42 @@
 				}
 		return $c;
 	}
-	//$visa=$_POST['visa'];
-	$visa="a012888888881881";
-	if(preg_match('/[0-9]/',$visa))
-		echo "cool</br>";
-	else 
-		echo "no cool";
-	$visa1=str_split($visa);
-	$cuenta=0;
-	$r=0;
-	for ($t=0;$t<=14;$t++)
-	{
-		if(($t%2)==0)
-		{
-			$co=$visa1[$t];
-			$co=$co*2;
-			$cuenta=$cuenta+$co;
-			if($co>=10)
-				$r=$r+1;
-			
-		}	
-		else //(t%2)!=0
-		{
-			$co=$visa1[$t];
-			$cuenta=$cuenta+$co;
-		}
-	}
-	$cuenta=$cuenta*-1;
-	$op=$cuenta-$r;
-	
-	$res=mod($op,10);
-	echo "</br>".$res;
-	
+	$visa=$_POST['visa'];
+	if(isset($_COOKIE['usuario'])
+	   {
+		if(preg_match('/[0-9]/',$visa))
+			echo "cool</br>";
+		else 
+			echo "no cool";
+		  if(strlen($visa)!=16)
+		  {
+			$visa1=str_split($visa);
+			$cuenta=0;
+			$r=0;
+			for ($t=0;$t<=14;$t++)
+			{
+				if(($t%2)==0)
+				{
+					$co=$visa1[$t];
+					$co=$co*2;
+					$cuenta=$cuenta+$co;
+					if($co>=10)
+						$r=$r+1;
+
+				}	
+				else //(t%2)!=0
+				{
+					$co=$visa1[$t];
+					$cuenta=$cuenta+$co;
+				}
+			}
+			$cuenta=$cuenta*-1;
+			$op=$cuenta-$r;
+
+			$res=mod($op,10);
+			echo "</br>".$res;
+		  }
+	   }
+	   else
+	   	echo "no tienes acceso";
 ?>
